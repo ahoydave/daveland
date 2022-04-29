c9adf544b135beee2bd05a6e4583ea6378fbeddc
David Jacka <ahoy.dave@gmail.com>
2022-04-29 20:04:22 +0100

    Get player moving around

--- src-client/index.ts
+++ src-client/index.ts
@@ -13,12 +13,15 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     },
     pixelArt: true
 };
 
 var game = new Phaser.Game(config);
+var cursors: any
+var player: any
 
 function preload ()
 {
@@ -43,14 +46,23 @@ function create ()
         repeat: -1
     })
     this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('link', { frames: [ 0 ]}),
+        frameRate: 13,
+        repeat: -1
+    })
+    this.anims.create({
         key: 'up',
         frames: this.anims.generateFrameNumbers('link', { frames: _.range(20, 30)}),
         frameRate: 13,
         repeat: -1
     })
-    const player = this.add.sprite(400, 300, 'link');
+    player = this.physics.add.sprite(400, 300, 'link');
+    player.body.allowGravity = false
     player.setScale(2)
-    player.play('up')
+    player.anims.play('down', true)
+    player.setCollideWorldBounds(true)
+
     let animIndex = 0;
     const anims = [ 'up', 'down', 'left' ]
     this.input.on('pointerdown', () => {
@@ -58,4 +70,41 @@ function create ()
         animIndex = animIndex % anims.length
         player.play(anims[animIndex])
     })
+
+    cursors = this.input.keyboard.createCursorKeys()
+}
+
+function update() {
+    let xVel = 0
+    let yVel = 0
+    if (cursors.left.isDown) {
+        xVel -= 160
+    }
+    if (cursors.right.isDown) {
+        xVel += 160
+    }
+    if (cursors.up.isDown) {
+        yVel -= 160
+    }
+    if (cursors.down.isDown) {
+        yVel += 160
+    }
+    player.setVelocityX(xVel)
+    player.setVelocityY(yVel)
+    if (yVel > 0) {
+        player.anims.play('down', true)
+        player.flipX = false
+    } else if (yVel < 0) {
+        player.anims.play('up', true)
+        player.flipX = false
+    } else if (xVel > 0) {
+        player.anims.play('left', true)
+        player.flipX = true
+    } else if (xVel < 0) {
+        player.anims.play('left', true)
+        player.flipX = false
+    } else {
+        player.anims.play('idle', true)
+        player.flipX = false
+    }
 }
